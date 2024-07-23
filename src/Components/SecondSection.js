36f69adaf9017bc2e0b43cc0eb674e295fbf7594
Hiba Obead <121480374+HibaObead@users.noreply.github.com>
2024-07-23 12:22:01 +0300

    Create SecondSection.js

---
+++ src/Components/SecondSection.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './SecondSection.css';
+import img3 from '../images/img3.svg';
+
+const SecondSection = () => {
+    return (
+        <section className='section'>
+            <div className='img'>
+                <img src={img3} alt="Illustration" />
+            </div>
+            <div className='text'>
+                <h1>Light, Fast & Powerful</h1>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
+                </p>
+            </div>
+        </section>
+    );
+};
+
+export default SecondSection;
